b84c92957e2a0f7b31d84fd985c397df6e54c13d
Rubens Farias <rubens.mbfarias@gmail.com>
2017-10-24 12:28:58 -0400

    Finish boiletplate.

--- nfs_fuse/client/client.c
+++ nfs_fuse/client/client.c
@@ -14,26 +14,22 @@ int main(void)
   int sfd;
   char* buf = "abcde";
 
-  ret = sfd = create_inet_stream_socket("127.0.0.1","55555",LIBSOCKET_IPv4,0);
+  ret = sfd = create_inet_stream_socket("127.0.0.1","1111",LIBSOCKET_IPv4,0);
 
-  if ( ret < 0 )
-  {
+  if (ret < 0) {
     perror(0);
     exit(1);
   }
 
   ret = write(sfd,buf,5);
-
-  if ( ret < 0 )
-  {
+  if (ret < 0) {
     perror(0);
     exit(1);
   }
 
   ret = destroy_inet_socket(sfd);
 
-  if ( ret < 0 )
-  {
+  if (ret < 0) {
     perror(0);
     exit(1);
   }
--- nfs_fuse/server/server.c
+++ nfs_fuse/server/server.c
@@ -4,46 +4,45 @@
 #include <string.h>
 #include <libsocket/libinetsocket.h>
 
-int main(void)
-{
+int doThing(int cfd) {
+  printf("hello from child!");
+  char bytes[300];
+  read(cfd, bytes, 300);
+  printf("%s\n", bytes);
+  return 0;
+}
+
+int main(void) {
   int sfd, bytes, ret;
-  char src_host[128], src_service[7], buf[16];
+  char src_host[128], src_port[7];
 
   src_host[127] = 0;
-  src_service[6] = 0;
+  src_port[6] = 0;
 
-  sfd = create_inet_server_socket("::", "1111", LIBSOCKET_TCP, LIBSOCKET_IPv6, 0);
+  sfd = create_inet_server_socket(
+      "::", "1111", LIBSOCKET_TCP, LIBSOCKET_IPv6, 0);
   if (sfd == -1) {
     perror("Couldn't create server");
     exit(1);
   }
   printf("Socket up and running\n");
 
-  while (1)
-  {
-    memset(buf,0,16);
-    int cfd = accept_inet_stream_socket(sfd,src_host,127,src_service,6,LIBSOCKET_NUMERIC,0);
+  while (1) {
+    int cfd = accept_inet_stream_socket(
+        sfd, src_host, 127, src_port, 6, LIBSOCKET_NUMERIC,0);
     if (cfd == -1) {
       perror("Couldn't accept connection");
       exit(1);
     }
-    printf("Received an incoming connection.\n");
 
-    bytes = read(cfd, buf, 15);
-
-    if ( ret < 0 )
-    {
-      perror(0);
-      exit(1);
+    printf("Connection from %s port %s.\n", src_host, src_port);
+    if (fork() == 0) {
+      doThing(cfd);
+      return 0;
     }
-
-    printf("Connection from %s port %s: %s (%i)\n",src_host,src_service,buf,bytes);
-    printf("Connection processed\n");
   }
 
-  ret = destroy_inet_socket(sfd);
-
-  if (ret < 0) {
+  if (destroy_inet_socket(sfd) < 0) {
     perror(0);
     exit(1);
   }
