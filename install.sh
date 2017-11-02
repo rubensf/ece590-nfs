1bbcd1abb2ed12a1265ea9ddf69b7a0a7def0dca
Rubens Farias <rubens.mbfarias@gmail.com>
2017-11-02 14:23:34 -0700

    Fix permissions for running

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@ git clone https://github.com/rubenstoshiro/ece590-nfs.git
 cd ece590-nfs/nfs_fuse
 git submodule init
 git submodule update
+chmod 775 make_client.sh
 bash make_client.sh
+chmod 775 make_server.sh
 bash make_server.sh
