b80693dd561c78f3fb28a2583cfddd93e8f12af4
Rubens Farias <rubens.mbfarias@gmail.com>
2017-11-02 14:36:41 -0700

    Update install script

--- install.sh
+++ install.sh
@@ -1,8 +1,21 @@
-sudo apt-get install -y libfuse-dev libsocket++ git
+# Some Prereqs
+sudo apt-get install -y libfuse-dev git cmake
+
+# Install libsocket
+git clone https://github.com/dermesser/libsocket.git
+cd libsocket
+cmake CMakeLists.txt
+make
+sudo make install
+cd ..
+sudo rm -rf libsocket
+
+# Install actual package
 git clone https://github.com/rubenstoshiro/ece590-nfs.git
 cd ece590-nfs/nfs_fuse
 git submodule init
 git submodule update
+mkdir build
 chmod 775 make_client.sh
 bash make_client.sh
 chmod 775 make_server.sh
