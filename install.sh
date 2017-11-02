3d5b08609f563290ffcc17291d215501056c337d
Rubens Farias <rubens.mbfarias@gmail.com>
2017-11-02 14:22:00 -0700

    Update submodules install script

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 sudo apt-get install -y libfuse-dev libsocket++ git
 git clone https://github.com/rubenstoshiro/ece590-nfs.git
 cd ece590-nfs/nfs_fuse
+git submodule init
+git submodule update
 bash make_client.sh
 bash make_server.sh
